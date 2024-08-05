127277277c02545809a35500209dcd503de18a96
Joe Bulfer <jbulfer13@gmail.com>
2024-08-05 11:52:25 -0500

    added total time function, just prints to console for now

--- timesheet/timesheet.rb
+++ timesheet/timesheet.rb
@@ -1,13 +1,19 @@
-#this code works, all that's left is to calculate total by subtracting time_in and time_out
 require 'csv'
 require 'time'
 
-# columns = ['date','time in','time out','total','comments']
-#
-# def time_diff_in_seconds(time1, time2)
-#   diff = Time.parse(time2) - Time.parse(time1)
-#   diff.round
-# end
+#TOTAL TIME FUNCTIONS
+def time_string_to_seconds(time_str)
+  hours, minutes, seconds = time_str.split(':').map(&:to_i)
+  hours * 3600 + minutes * 60 + seconds
+end
+
+def seconds_to_time_string(seconds)
+  hours = seconds / 3600
+  minutes = (seconds % 3600) / 60
+  seconds = seconds % 60
+  format("%02d:%02d:%02d", hours, minutes, seconds)
+end
+#TOTAL TIME FUNCTIONS END
 
 def clock_in()
     current_date = Date.today.strftime("%m-%d-%Y")
@@ -17,7 +23,7 @@ def clock_in()
   
     data_row = [current_date, current_time, '', '', comment,'']
   
-    CSV.open("timesheet.csv", 'a') { |csv| csv << data_row }
+    CSV.open("total-test.csv", 'a') { |csv| csv << data_row }
   end
 
 def clock_out()
@@ -26,7 +32,7 @@ def clock_out()
     print "Enter Out Comment > "
     comment = gets.chomp
   
-    csv_data = CSV.read("timesheet.csv")
+    csv_data = CSV.read("total-test.csv")
   
     last_row = csv_data[-1]
     last_row[2] = current_time 
@@ -40,27 +46,34 @@ def clock_out()
 
     last_row[3] = total 
   
-    CSV.open("timesheet.csv", "w") do |csv|
+    CSV.open("total-test.csv", "w") do |csv|
       csv_data.each do |row|
         csv << row
       end
     end
   end
 
-def find_total_time() #add "Total" colunm into single cell at bottom
-  csv_data = CSV.read("timesheet.csv").transpose # transpose turns rows into columns
+def find_total_time() #add "Total" colunm into single cell at bottom (for now it just prints to console)
+  csv_data = CSV.read("total-test.csv").transpose # transpose turns rows into columns
   total_column = csv_data[3] 
-  p total_column.slice(1..) #ignore the header
+  total_column = total_column.slice(1..)#ignore the header
+
+  total_seconds = total_column.inject(0) do |sum, time_str|
+    sum + time_string_to_seconds(time_str)
+  end
+
+  total_time_string = seconds_to_time_string(total_seconds)
+
+  puts "Total Time: #{total_time_string}" #TODO: write this to a cell
 end
 
-#I probly worked an hour on this before I went to lunch
-
-#find_total_time
-puts "Clock in or out? i/o? > "
+puts "Clock in or out? i/o?\nOr f for find_total_time"
 option = gets.chomp.downcase
 if option == "i"
     clock_in
 elsif option == "o"
     clock_out
+elsif option == "f"
+    find_total_time
 end
 
